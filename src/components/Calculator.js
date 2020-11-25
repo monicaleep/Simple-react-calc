3938137f88653d3b1813040476e3d9bb56c02f06
Monica <monica.parrillo@gmail.com>
2020-11-24 20:43:29 -0600

    Complete step 3 - change state when numbers entered into inputs

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,14 +6,29 @@ class Calculator extends Component {
       num2:0,
       result: 0
     }
+    setNum = (e, num) =>{
+      this.setState({
+        [num]: parseInt(e.target.value)
+      })
+    }
     render() {
         return (
           <div className="container">
             <h1>Add with React!</h1>
             <div className="add">
-              <input type="text" />
+              <input type="number"
+                name="num1"
+                placeholder="Enter your first number"
+                value={this.state.num1}
+                onChange={(e)=> this.setNum(e,'num1')}
+                />
               <span>+</span>
-              <input type="text" />
+              <input type="number"
+                name="num2"
+                placeholder="Enter 2nd number"
+                value={this.state.num2}
+                onChange={(e)=> this.setNum(e,'num2')}
+               />
               <button>=</button>
               <h3>Addition results go here!</h3>
             </div>
